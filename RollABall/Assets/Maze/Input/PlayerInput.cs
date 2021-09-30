da70dd928ed7173373c0172a7c2c0b5499c493f4
SamVanElst <samvanelst.school@gmail.com>
2021-09-30 09:46:20 +0200

    movemnt

--- RollABall/Assets/Maze/Input/PlayerInput.cs
+++ RollABall/Assets/Maze/Input/PlayerInput.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Input/PlayerInput.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Maze/Input/PlayerInput.inputactions'
 
 using System;
 using System.Collections;
